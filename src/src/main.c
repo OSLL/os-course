32b527a6c1a362ed9abeaa4c3ea0c5dbc86890cd
Mike Krinkin <krinkin.m.u@gmail.com>
2016-11-02 18:54:12 +0300

    add some basic tests

--- src/src/main.c
+++ src/src/main.c
@@ -2,7 +2,9 @@
 #include <memory.h>
 #include <balloc.h>
 #include <paging.h>
+#include <debug.h>
 #include <alloc.h>
+#include <print.h>
 #include <ints.h>
 #include <time.h>
 
@@ -13,6 +15,92 @@ static void qemu_gdb_hang(void)
 
 	while (wait);
 #endif
+}
+
+static void test_alloc(void)
+{
+	struct list_head head;
+	unsigned long count = 0;
+
+	list_init(&head);
+	while (1) {
+		struct list_head *node = mem_alloc(sizeof(*node));
+
+		if (!node)
+			break;
+		BUG_ON((uintptr_t)node < HIGHER_BASE);
+		++count;
+		list_add(node, &head);
+	}
+
+	printf("Allocated %lu bytes\n", count * sizeof(head));
+
+	while (!list_empty(&head)) {
+		struct list_head *node = head.next;
+
+		BUG_ON((uintptr_t)node < HIGHER_BASE);
+		list_del(node);
+		mem_free(node);
+	}
+
+	mem_alloc_shrink();
+}
+
+static void test_slab(void)
+{
+	struct list_head head;
+	struct mem_cache cache;
+	unsigned long count = 0;
+
+	list_init(&head);
+	mem_cache_setup(&cache, sizeof(head), sizeof(head));
+	while (1) {
+		struct list_head *node = mem_cache_alloc(&cache);
+
+		if (!node)
+			break;
+		BUG_ON((uintptr_t)node < HIGHER_BASE);
+		++count;
+		list_add(node, &head);
+	}
+
+	printf("Allocated %lu bytes\n", count * sizeof(head));
+
+	while (!list_empty(&head)) {
+		struct list_head *node = head.next;
+
+		BUG_ON((uintptr_t)node < HIGHER_BASE);
+		list_del(node);
+		mem_cache_free(&cache, node);
+	}
+
+	mem_cache_release(&cache);
+}
+
+static void test_buddy(void)
+{
+	struct list_head head;
+	unsigned long count = 0;
+
+	list_init(&head);
+	while (1) {
+		struct page *page = __page_alloc(0);
+
+		if (!page)
+			break;
+		++count;
+		list_add(&page->ll, &head);
+	}
+
+	printf("Allocated %lu pages\n", count);
+
+	while (!list_empty(&head)) {
+		struct list_head *node = head.next;
+		struct page *page = CONTAINER_OF(node, struct page, ll);
+
+		list_del(&page->ll);
+		__page_free(page, 0);
+	}
 }
 
 void main(void *bootstrap_info)
@@ -27,6 +115,10 @@ void main(void *bootstrap_info)
 	page_alloc_setup();
 	mem_alloc_setup();
 	enable_ints();
+
+	test_buddy();
+	test_slab();
+	test_alloc();
 
 	while (1);
 }
