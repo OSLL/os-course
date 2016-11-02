084f0fa17b75052b3ab35b37599b67a00ca7d227
Mike Krinkin <krinkin.m.u@gmail.com>
2016-11-02 15:52:44 +0300

    memory map parsing and printing

--- src/src/main.c
+++ src/src/main.c
@@ -1,4 +1,5 @@
 #include <serial.h>
+#include <balloc.h>
 #include <ints.h>
 #include <time.h>
 
@@ -11,13 +12,14 @@ static void qemu_gdb_hang(void)
 #endif
 }
 
-void main(void)
+void main(void *bootstrap_info)
 {
 	qemu_gdb_hang();
 
 	serial_setup();
 	ints_setup();
 	time_setup();
+	balloc_setup(bootstrap_info);
 	enable_ints();
 
 	while (1);
